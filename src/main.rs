e8b6632b1fbb70ef9ac3817558a2d5ccf77e14e9
jingQian431 <37545839+jingQian431@users.noreply.github.com>
2019-05-10 18:17:17 +0200

    implemented a struct for guess number, and the guess number must be between 1 and 100. If not occurs panic

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value:i32) -> Guess{
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+        Guess {
+            value
+        }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -16,17 +35,17 @@ fn main() {
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(e) => {
                 println!("{}", e);
                 continue;
             },
         };
 
-        println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess.value);
 
-        match guess.cmp(&secret_number) {
+        match guess.value.cmp(&secret_number) {
             Ordering::Less => println!("Too small"),
             Ordering::Equal => {
                 println!("You win");
